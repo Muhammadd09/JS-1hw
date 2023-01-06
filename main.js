8b806a1b5169c0f986567a22d1a9afa48f15ddf6
Muhammad <muzammadshuhratov@gmail.com>
2023-01-06 19:34:16 +0500

    JS-1hw

--- main.js
+++ main.js
@@ -5,8 +5,24 @@ console.log('Sizning Ismingiz ' + str);
 
 
 var num = +prompt('Misolni yeching: 7 * 7 = ? ')
-alert(' Sizning Javobingiz ' + num + ' Togri Javob esa: ' + (7 * 7))
+alert(' Sizning Javobingiz ' + num + ' Togri javob esa: ' + (7 * 7))
 console.log('1) 7 * 7 = ' + (7 * 7) + ' Sizning javobingiz: ' + num);
 
 
-alert('Javoblaringizni korish uchun consolni oching')
+var num = +prompt('Misolni yeching: 20 / 4 = ?')
+alert(' Sizning Javobingiz ' + num + ' Togri javob esa ' + num)
+console.log('2) 20 / 4 = ' + (20 / 4) + 'Sizning javobingiz: ' + num);
+
+
+var num = +prompt('Misolni yeching: 8 + 8 = ?' )
+alert('Sizning javobingiz ' + num + ' Togri javob esa ' + (8 + 8))
+console.log('3) 8 + 8 =' + (8 + 8) + 'Sizning javonimgiz ' + num);
+
+
+
+var num = +prompt('Misolni yeching 11 % 2 = ?')
+alert('Sizning javobingiz ' + num + ' Togri javob esa ' + (11 % 2))
+console.log('5) 11 % 2 =' + (11 % 2) + 'Sizning javobingiz ' + num);
+
+
+// alert('Javoblaringizni korish uchun consolni oching')  
