9d6cf0f87637d6b7b90bfc8065fe3e1ff70aafda
Muhammad <muzammadshuhratov@gmail.com>
2023-01-06 09:47:46 +0500

    JS-1hw

---
+++ main.js
@@ -0,0 +1,12 @@
+var str = prompt('Ismingizni kiriting ')
+
+alert('Hush Kelibsiz ' + str)
+console.log('Sizning Ismingiz ' + str);
+
+
+var num = +prompt('Misolni yeching: 7 * 7 = ? ')
+alert(' Sizning Javobingiz ' + num + ' Togri Javob esa: ' + (7 * 7))
+console.log('1) 7 * 7 = ' + (7 * 7) + ' Sizning javobingiz: ' + num);
+
+
+alert('Javoblaringizni korish uchun consolni oching')
